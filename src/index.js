38703eb7e957a953797b498a0eaf6dfa9d2b7957
Rebecca Wiedau <rebecca.wiedau@nrwsoft.de>
2021-06-11 14:22:32 +0200

    Refactor appData and date conversion

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 //DATE
 let degreeUnitButton= document.querySelector("#degree-unit-button");
-function currentDate() {
-  let weekdayEl = document.querySelector(".today__weekday");
-  let dateEl = document.querySelector(".today__day");
-  let currentTimeEl = document.querySelector(".today__current-time");
-  let now = new Date();
+
+function twoDigitFormat (number) {
+  if (number < 10) {
+    number = `0${number}`;
+  }
+  return number;
+}
+
+function convertTimestampToDate(timestamp) {
   let weekdays = [
     "Sun,",
     "Mon,",
@@ -14,35 +18,50 @@ function currentDate() {
     "Fri,",
     "Sat,"
   ];
-  let weekday = weekdays[now.getDay()];
-  let minutes = now.getMinutes();
-  let hour = now.getHours();
-  let day = now.getDate() + 1;
-  if (day < 10) {
-    day = `0${day}`;
-  }
-  let month = now.getMonth() + 1;
-  if (month < 10) {
-    month = `0${month}`;
-  }
 
-  let year = now.getFullYear();
-  dateEl.innerHTML = `${day}.${month}.${year}`;
-  weekdayEl.innerHTML = `${weekday}`;
-  currentTimeEl.innerHTML = `${hour}:${minutes}`;
+  let date = {
+    weekday: weekdays[timestamp.getDay()],
+    minutes: timestamp.getMinutes(),
+    hour: timestamp.getHours(),
+    day: timestamp.getDate() + 1,
+    month: timestamp.getMonth() + 1,
+    year: timestamp.getFullYear()
+  };
+
+  date.minutes = twoDigitFormat(date.minutes);
+  date.hour = twoDigitFormat(date.hour);
+  date.month = twoDigitFormat(date.month);
+  date.day = twoDigitFormat(date.day);
+
+return date;
+}
+
+function currentDate() {
+  let weekdayEl = document.querySelector(".today__weekday");
+  let dateEl = document.querySelector(".today__day");
+  let currentTimeEl = document.querySelector(".today__current-time");
+  let now = convertTimestampToDate(new Date());
+
+  dateEl.innerHTML = `${now.day}.${now.month}.${now.year}`;
+  weekdayEl.innerHTML = `${now.weekday}`;
+  currentTimeEl.innerHTML = `${now.hour}:${now.minutes}`;
 }
 
 currentDate();
 
 // SET THE WEATHER TO HTML
-
+function setWeatherIcon(weatherID, weatherDescription) {
+  let currentWeatherIcon = document.querySelector("#today__weather-icon");
+  currentWeatherIcon.innerHTML = '<img src="media/icons/cloud-showers-heavy-solid.svg"/>';
+}
 function setWeather(response) {
   let appData = {
     location: {
       city: response.data.name,
       countryCode: response.data.sys.country,
       longitude: response.data.coord.lon,
-      latitude: response.data.coord.lat
+      latitude: response.data.coord.lat,
+      timestamp: response.data.dt * 1000
     },
     weather: {
       temp: {
@@ -52,16 +71,18 @@ function setWeather(response) {
       },
       humidity: response.data.main.humidity,
       windSpeed: response.data.wind.speed,
-      icon: response.data.weather[0].icon
+      id: response.data.weather[0].id,
+      description: response.data.weather[0].description
     }
   };
+
   console.log(appData);
   let weatherCity = document.querySelector("#weather-city");
   let weatherCountry = document.querySelector("#weather-country");
   let currentTemp = document.querySelector("#current-temp__value");
   let currentHumidity = document.querySelector("#current-humidity__value");
   let currentWindSpeed = document.querySelector("#current-wind-speed__value");
-  let currentWeatherIcon = document.querySelector("#today__weather-icon");
+
   let todayMaxTemp = document.querySelector(".today__max-temp");
   let todayMinTemp = document.querySelector("#today__min-temp-value");
   weatherCity.innerHTML = appData.location.city;
@@ -71,11 +92,10 @@ function setWeather(response) {
   currentWindSpeed.innerHTML = `${appData.weather.windSpeed}`;
   todayMinTemp.innerHTML = Math.round(appData.weather.temp.min);
   todayMaxTemp.innerHTML = Math.round(appData.weather.temp.max);
+
   //TODO SET ICON
 
-  // currentWeatherIcon.attributes[
-  //   "src"
-  // ].value = `https://openweathermap.org/img/wn/${appData.weather.icon}@2x.png`;
+ setWeatherIcon(appData.weather.id, appData.weather.description);
 }
 
 //CITY SEARCH
