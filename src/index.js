b0b57525671f939b10d44b469346abc8de535930
Rebecca Wiedau <rebecca.wiedau@nrwsoft.de>
2021-06-11 13:27:59 +0200

    add calculation to changeUnit and improve api-call

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 //DATE
-
+let degreeUnitButton= document.querySelector("#degree-unit-button");
 function currentDate() {
   let weekdayEl = document.querySelector(".today__weekday");
   let dateEl = document.querySelector(".today__day");
@@ -80,8 +80,9 @@ function setWeather(response) {
 
 //CITY SEARCH
 function weatherByCity(searchedCity) {
+  let unit = degreeUnitButton.getAttribute("value").valueOf();
   let apiKey = "3706e2853360265ffac41fac1cf2f67c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&units=metric&appid=${apiKey}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&units=${unit}&appid=${apiKey}`;
   axios.get(apiUrl).then(setWeather);
 }
 
@@ -102,39 +103,59 @@ searchForm.addEventListener("submit", searchCity);
 // CURRENT
 
 function currentLocationWeather(position) {
-  console.log("currentLocationWeather");
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
+  let unit = degreeUnitButton.getAttribute("value").valueOf();
   let apiKey = "3706e2853360265ffac41fac1cf2f67c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&$units=${unit}&appid=${apiKey}`;
   axios.get(apiUrl).then(setWeather);
 }
 
 function currentLocation() {
-  console.log("currentLocation");
   navigator.geolocation.getCurrentPosition(currentLocationWeather);
 }
 
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", currentLocation);
 
+function convertTemperatureToFahrenheit() {
+  let temperatureValues = document.querySelectorAll(".temperature");
+  console.log(temperatureValues);
+  console.log(temperatureValues[0].innerHTML.valueOf());
+  console.log(temperatureValues[0].innerHTML.valueOf() * 1.8 + 32);
+  temperatureValues.forEach(value => {
+    value.innerHTML = `${Math.round(value.innerHTML.valueOf() * 1.8 + 32)}`;
+  })
+}
+
+function convertTemperatureToCelsius() {
+  let temperatureValues = document.querySelectorAll(".temperature");
+  console.log(temperatureValues);
+  console.log(temperatureValues[0].innerHTML.valueOf());
+  console.log(temperatureValues[0].innerHTML.valueOf() * 1.8 + 32);
+  temperatureValues.forEach(value => {
+    value.innerHTML = `${Math.round((value.innerHTML.valueOf() -32) / 1.8)}`;
+  })
+}
 
 function changeUnit() {
   let changingUnit = this.value;
   let degreeSigns = document.querySelectorAll(".degree-sign");
   let buttonDegreeSign = document.querySelector(".button-degree-sign");
 
-  if (changingUnit === "fahrenheit") {
+  if (changingUnit === "metric") {
     degreeSigns.forEach(degreeSign => { degreeSign.innerHTML = "°F"});
     buttonDegreeSign.innerHTML = "℃";
-    this.value = "celsius";
-  } else if (changingUnit === "celsius") {
+    convertTemperatureToFahrenheit();
+    this.value = "imperial";
+  } else if (changingUnit === "imperial") {
     degreeSigns.forEach(degreeSign => { degreeSign.innerHTML = "℃"});
     buttonDegreeSign.innerHTML = "°F";
-    this.value = "fahrenheit";
+    convertTemperatureToCelsius();
+    this.value = "metric";
   }
 }
 
-let degreeUnitButton= document.querySelector("#degree-unit-button");
+
 
 degreeUnitButton.addEventListener("click", changeUnit);
